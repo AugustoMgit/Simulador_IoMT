43b2c18dbb50cabd0ca1a83b8550089d16940dd1
William <178344@upf.br>
2021-11-04 11:24:16 -0300

    SPO2 gerando valores aleatorios

---
+++ utils/SP02.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def FrequenciaCardiaca(qtValores):
+    normais = int(qtValores * 0.8)
+    anormais = int(qtValores * 0.2)
+
+    # Gerando os valores normais
+    valoresNormais = np.random.uniform(50, 100, normais)
+
+    # Gerando os valores anormais
+    valoresAnormais1 = np.random.uniform(0, 49, int(anormais / 2))
+    valoresAnormais2 = np.random.uniform(101, 200, int(anormais / 2))
+    valoresAnormais = np.concatenate((valoresAnormais1, valoresAnormais2))
+
+    return(valoresNormais, valoresAnormais)
+
+
+def SP02(qtValores, minMinutos, MaxMinutos):
+    normais = int(qtValores * 0.8)
+    anormais = int(qtValores * 0.2)
+
+    # Gerando os valores normais
+    valoresNormais = np.random.uniform(90, 100, normais)
+
+    # Gerando os valores anormais
+    valoresAnormais1 = np.random.uniform(50, 89, int(anormais / 2))
+    valoresAnormais2 = np.random.uniform(0, 50, int(anormais / 2))
+    valoresAnormais = np.concatenate((valoresAnormais1, valoresAnormais2))
+
+    frequenciaCardiaca = FrequenciaCardiaca(qtValores)
+    frequenciaCardiacaNormais = frequenciaCardiaca[0]
+    frequenciaCardiacaAnormais = frequenciaCardiaca[1]
+
+
+SP02(40, 0, 60)
--- utils/TC.py
+++ utils/TC.py
@@ -1,9 +1,8 @@
-import random
 import numpy as np
 
-def TC(qtValores, minMinutos, MaxMinutos):
-    normais = int((qtValores * 80) / 100)
-    anormais = int((qtValores * 20) / 100)
+def TemperaturaCorporal(qtValores, minMinutos, MaxMinutos):
+    normais = int(qtValores * 0.8)
+    anormais = int(qtValores * 0.2)
 
     # Gerando os valores normais
     valoresNormais = np.random.uniform(36, 37.5, normais)
@@ -16,4 +15,4 @@ def TC(qtValores, minMinutos, MaxMinutos):
     print(valoresAnormais)
 
 
-TC(40, 0, 60)
+TemperaturaCorporal(40, 0, 60)
