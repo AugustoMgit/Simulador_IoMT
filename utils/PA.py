587287388d2f1b5dd9b7eca3e397c37ca3c11a35
William <178344@upf.br>
2021-11-04 15:11:42 -0300

    gerando datetime aleatoria para o metodo PA

--- utils/PA.py
+++ utils/PA.py
@@ -1,13 +1,21 @@
 import numpy as np
+import random
+from datetime import datetime, timedelta, time
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def PA(qtValores, minMinutos, MaxMinutos):
     normais = int(qtValores * 0.8)
     anormais = int(qtValores * 0.2)
 
+    minutes = 0
+    today = datetime.today()
+    date = datetime(today.year, today.month, today.day, 8, 30, 0)
+
     # Gerando os valores normais
     sistolicaNormal = np.random.randint(110, 129, normais)
     diastolicaNormal = np.random.randint(70, 84, normais)
-    print(sistolicaNormal, diastolicaNormal)
+    pressaoNormal = np.concatenate((sistolicaNormal, diastolicaNormal))
 
     # Gerando os valores anormais
     sistolicaAnormais1 = np.random.randint(0, 109, int(anormais / 2))
@@ -18,7 +26,26 @@ def PA(qtValores, minMinutos, MaxMinutos):
     diastolicaAnormais2 = np.random.randint(85, 300, int(anormais / 2))
     diastolicaAnormais = np.concatenate((diastolicaAnormais1, diastolicaAnormais2))
 
-    print(sistolicaAnormais, diastolicaAnormais)
+    pressaoAnormal = np.concatenate((sistolicaAnormais, diastolicaAnormais))
+
+    # Gerando os valores de PA
+    valores = np.concatenate((pressaoNormal, pressaoAnormal))
+    np.random.shuffle(valores)
+
+    dataPlot = []
+    for i in range(len(valores)):
+        valores[i] = round(valores[i], 2)
+        minutes +=random.randint(minMinutos, MaxMinutos)
+        delta = timedelta(minutes=minutes)
+        dateTime = (date + delta)
+        dataPlot.append(tuple([dateTime, valores[i]]))
+
+    df = pd.DataFrame(dataPlot, columns=['Data', 'PA'])
+    plt.plot(df['Data'], df['PA'])
+    plt.ylabel("%")
+    plt.xlabel("Hora")
+    plt.legend()
+    plt.savefig('../images/plots/PA.png')
 
 
-#PA(80, 30, 120)
+PA(20, 30, 120)
