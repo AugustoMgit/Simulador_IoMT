f16d423d18461d2feb8dbcccc94c9b32b64f7b4b
William <178344@upf.br>
2021-11-04 14:49:46 -0300

    gerando datetime aleatoria para o metodo SP02

--- utils/SP02.py
+++ utils/SP02.py
@@ -1,4 +1,8 @@
 import numpy as np
+import random
+from datetime import datetime, timedelta, time
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def FrequenciaCardiaca(qtValores):
     normais = int(qtValores * 0.8)
@@ -19,6 +23,10 @@ def SP02(qtValores, minMinutos, MaxMinutos):
     normais = int(qtValores * 0.8)
     anormais = int(qtValores * 0.2)
 
+    minutes = 0
+    today = datetime.today()
+    date = datetime(today.year, today.month, today.day, 8, 30, 0)
+
     # Gerando os valores normais
     valoresNormais = np.random.uniform(90, 100, normais)
 
@@ -28,8 +36,30 @@ def SP02(qtValores, minMinutos, MaxMinutos):
     valoresAnormais = np.concatenate((valoresAnormais1, valoresAnormais2))
 
     frequenciaCardiaca = FrequenciaCardiaca(qtValores)
-    frequenciaCardiacaNormais = frequenciaCardiaca[0]
-    frequenciaCardiacaAnormais = frequenciaCardiaca[1]
+    frequenciaCardiaca = np.concatenate((frequenciaCardiaca[0], frequenciaCardiaca[1]))
 
+    # Gerando os valores de SP02
+    valores = np.concatenate((valoresNormais, valoresAnormais))
+    np.random.shuffle(valores)
 
-#SP02(40, 0, 60)
+    dataPlot = []
+    for i in range(len(valores)):
+        valores[i] = round(valores[i], 2)
+        frequenciaCardiaca[i] = round(frequenciaCardiaca[i], 2)
+        minutes +=random.randint(minMinutos, MaxMinutos)
+        delta = timedelta(minutes=minutes)
+        dateTime = (date + delta)
+
+        dataPlot.append(tuple((dateTime, valores[i], frequenciaCardiaca[i])))
+
+    df = pd.DataFrame(dataPlot, columns=['Data', 'SP02', 'FrequenciaCardiaca'])
+
+    plt.plot(df.Data, df.SP02, label='SP02', color = 'red')
+    plt.plot(df.Data, df.FrequenciaCardiaca, label='Frequencia Cardiaca', color = 'blue')
+
+    plt.ylabel("%")
+    plt.xlabel("Hora")
+    plt.legend()
+    plt.savefig('../images/plots/SP02.png')
+
+SP02(20, 5, 60)
